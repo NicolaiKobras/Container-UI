463783fb24ce01d40904e6adc35a9e2c185df1a3
Nicolai Kobras <Nicolaikobras01@gmail.com>
2025-09-10 14:16:03 +0200

    Initial Commit

---
+++ ContainerUI/ContainerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContainerUIApp.swift
+//  ContainerUI
+//
+//  Created by Nicolai Kobras on 10.09.25.
+//
+
+import SwiftUI
+
+@main
+struct ContainerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
